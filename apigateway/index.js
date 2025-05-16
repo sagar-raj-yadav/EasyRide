d8331558237c7a6aebdd97afd0dac936040472b0
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2025-05-17 02:49:59 +0530

    Update index.js

--- apigateway/index.js
+++ apigateway/index.js
@@ -8,11 +8,7 @@ const port= 1000;
 app.use(cors());
 
 
-const services={
-    userService: 'http://localhost:5001',
-    busService: 'http://localhost:5002',
-    bookingService: 'http://localhost:5003',
-}
+
 
 app.get('/', (req, res) => {
     res.json('Hello, world');
